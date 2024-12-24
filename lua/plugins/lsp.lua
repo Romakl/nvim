2774e8281c3d72839e84f7e1b608e958e906877a
Roman Kliuiev <romakl55555@gmail.com>
2024-12-24 22:32:29 +0200

    refactor(lsp): simplify server setup logic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,80 +1,102 @@
-
--- Setup language servers.
+-- Import lspconfig module
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {
-	settings = {
-	    pyright = {
-	      -- Using Ruff's import organizer
-	      disableOrganizeImports = true,
-	    },
-	    python = {
-	      analysis = {
-	        -- Ignore all files for analysis to exclusively use Ruff for linting
-	        ignore = { '*' },
-	        },
-		},
-	},	
-}
+local util = require('lspconfig.util') -- Import lspconfig util for root patterns
 
-lspconfig.ts_ls.setup({})
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
+-- Helper function to set up language servers with common settings
+local function setup_server(server_name, config)
+  lspconfig[server_name].setup(config or {})
+end
+
+-- General LSP settings
+local default_on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr }
+
+  -- Enable completion triggered by <c-x><c-o>
+  vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+  -- Buffer-local mappings for LSP actions
+  vim.keymap.set('n', 'lD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'ld', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'lk', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'li', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set({ 'n', 'v' }, '<space>r', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<space>f', function()
+    vim.lsp.buf.format { async = true }
+  end, opts)
+end
+
+-- Pyright (Python) Configuration
+setup_server('pyright', {
+  settings = {
+    pyright = {
+      disableOrganizeImports = true, -- Use Ruff for import organization
+    },
+    python = {
+      analysis = {
+        ignore = { '*' }, -- Ignore all files for analysis to exclusively use Ruff
+      },
+    },
+  },
+  on_attach = default_on_attach,
+})
+
+-- TypeScript/JavaScript Configuration
+setup_server('ts_ls', {
+  on_attach = default_on_attach,
+})
+
+-- Rust Analyzer Configuration
+setup_server('rust_analyzer', {
   settings = {
     ['rust-analyzer'] = {},
   },
-}
+  on_attach = default_on_attach,
+})
 
--- Setup Ruff Linter
-lspconfig.ruff.setup {
+-- Go (gopls) Configuration
+setup_server('gopls', {
+  on_attach = default_on_attach,
+  capabilities = lspconfig.util.default_config.capabilities, -- Add capabilities if needed
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+})
+
+-- Ruff Linter Configuration
+setup_server('ruff', {
   init_options = {
     settings = {
-      -- Any extra CLI arguments for `ruff` go here.
       args = {
-		"--select=E,F,UP,N,I,ASYNC,S,PTH",
-		"--line-length=79",
-		"--respect-gitignore",  -- Исключать из сканирования файлы в .gitignore
-      	"--target-version=py311"
+        "--select=E,F,UP,N,I,ASYNC,S,PTH",
+        "--line-length=79",
+        "--respect-gitignore", -- Exclude files in .gitignore
+        "--target-version=py311",
       },
-    }
-  }
-}
+    },
+  },
+  on_attach = default_on_attach,
+})
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- Diagnostic Key Mappings
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
+-- LSP Attach Autocommand
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'lD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'ld', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'lk', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'i', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, opts)
-    
-    -- TODO: Используется повторно, необходимо вырезать в след.версии
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>r', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
+    default_on_attach(ev.client, ev.buf)
   end,
 })
